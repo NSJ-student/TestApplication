284c0375630523b347088acd933787d02f153f0d
NSJ-student <ghdlghdl0518@hanmail.net>
2023-01-26 19:01:04 +0900

    fixing error

--- app/src/main/java/com/example/emptyapplication/MainActivity.java
+++ app/src/main/java/com/example/emptyapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.emptyapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.graphics.Color;
 import android.os.Bundle;
@@ -12,11 +13,20 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
 import java.util.ArrayList;
+
+import com.example.emptyapplication.ui.main.SectionsPagerAdapter;
+import com.example.emptyapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    int count;
+    private ActivityMainBinding binding;
+
+    private int count;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,48 +34,22 @@ public class MainActivity extends AppCompatActivity {
         // hide appbar
         getSupportActionBar().hide();
 
-        count = 0;
-        // 빈 데이터 리스트 생성.
-        final ArrayList<String> items = new ArrayList<String>() ;
-        // ArrayAdapter 생성. 아이템 View를 선택(single choice)가능하도록 만듦.
-        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, items) ;
-        // listview 생성 및 adapter 지정.
-        final ListView devList = (ListView) findViewById(R.id.deviceList) ;
-        devList.setAdapter(adapter) ;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        Button btnRefresh = (Button) findViewById(R.id.btnRefresh) ;
-        btnRefresh.setOnClickListener(new Button.OnClickListener() {
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = binding.fab;
+
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Toast...", Toast.LENGTH_SHORT).show();
-                System.out.println("toast Refresh");
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
-        }) ;
-
-        Button btnSelect = (Button) findViewById(R.id.btnSelect) ;
-        btnSelect.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                count++;
-
-                Log.i("TEST", "Append " + Integer.toString(count));
-                System.out.println("toast Append " + Integer.toString(count));
-                // 아이템 추가.
-                items.add("LIST" + Integer.toString(count));
-                // listview 갱신
-                adapter.notifyDataSetChanged();
-            }
-        }) ;
-
-        final TextView inputText = (TextView) findViewById(R.id.singleLineInput);
-        final TextView textLog = (TextView) findViewById(R.id.textLog);
-        Button btnEnter = (Button) findViewById(R.id.btnEnter) ;
-        btnEnter.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                textLog.append(inputText.getText() + "\n");
-                inputText.setText("") ;
-            }
-        }) ;
+        });
     }
 }

fba0c587fc4d3ec596c657be42ed381af1c2b808
NSJ-student <ghdlghdl0518@hanmail.net>
2023-01-30 17:02:19 +0900

    다른 블루투스 예제 참고 작업중

--- app/src/main/java/com/example/emptyapplication/BluetoothFragment.java
+++ app/src/main/java/com/example/emptyapplication/BluetoothFragment.java
@@ -1,19 +1,34 @@
 package com.example.emptyapplication;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -27,6 +42,24 @@ public class BluetoothFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
     private int count;
+
+    private static final UUID BT_MODULE_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); // "random" unique identifier
+
+    // #defines for identifying shared types between calling functions
+    private final static int REQUEST_ENABLE_BT = 1; // used to identify adding bluetooth names
+    public final static int MESSAGE_READ = 2; // used in bluetooth handler to identify message update
+    private final static int CONNECTING_STATUS = 3; // used in bluetooth handler to identify message status
+
+    private Handler mHandler; // Our main handler that will receive callback notifications
+    private ConnectedThread mConnectedThread; // bluetooth background worker thread to send and receive data
+    private BluetoothSocket mBTSocket = null; // bi-directional client-to-client data path
+
+    private BluetoothAdapter mBTAdapter;
+    private Set<BluetoothDevice> mPairedDevices;
+    private ArrayAdapter<String> mBTArrayAdapter;
+    private static ListView mDevicesListView;
+    private static Button btnRefresh;
+    private static Button btnSelect;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -62,7 +95,13 @@ public class BluetoothFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
 
+        count = 0;
+        // ArrayAdapter 생성. 아이템 View를 선택(single choice)가능하도록 만듦.
+        mBTArrayAdapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1) ;
+        // get a handle on the bluetooth radio
+        mBTAdapter = BluetoothAdapter.getDefaultAdapter();
 
+        bluetoothOn();
     }
 
     @Override
@@ -71,25 +110,22 @@ public class BluetoothFragment extends Fragment {
 
         View v = inflater.inflate(R.layout.fragment_bluetooth, container, false);
 
-        count = 0;
-        // 빈 데이터 리스트 생성.
-        final ArrayList<String> items = new ArrayList<String>() ;
-        // ArrayAdapter 생성. 아이템 View를 선택(single choice)가능하도록 만듦.
-        final ArrayAdapter adapter = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_single_choice, items) ;
-        // listview 생성 및 adapter 지정.
-        final ListView devList = (ListView) v.findViewById(R.id.deviceList) ;
-        devList.setAdapter(adapter) ;
+        // GUI Id 가져오기
+        mDevicesListView = (ListView) v.findViewById(R.id.deviceList) ;
+        btnRefresh = (Button) v.findViewById(R.id.btnRefresh) ;
+        btnSelect = (Button) v.findViewById(R.id.btnSelect) ;
+        //  adapter 지정.
+        mDevicesListView.setAdapter(mBTArrayAdapter) ;
+        mDevicesListView.setOnItemClickListener(mDeviceClickListener);
 
-        Button btnRefresh = (Button) v.findViewById(R.id.btnRefresh) ;
         btnRefresh.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getContext(), "Toast...", Toast.LENGTH_SHORT).show();
-                System.out.println("toast Refresh");
+                listPairedDevices();
+                Toast.makeText(getContext(), "Paired Device...", Toast.LENGTH_SHORT).show();
             }
         }) ;
 
-        Button btnSelect = (Button) v.findViewById(R.id.btnSelect) ;
         btnSelect.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -98,12 +134,113 @@ public class BluetoothFragment extends Fragment {
                 Log.i("TEST", "Append " + Integer.toString(count));
                 System.out.println("toast Append " + Integer.toString(count));
                 // 아이템 추가.
-                items.add("LIST" + Integer.toString(count));
+                mBTArrayAdapter.add("LIST" + Integer.toString(count));
                 // listview 갱신
-                adapter.notifyDataSetChanged();
+                // mBTArrayAdapter.notifyDataSetChanged();
             }
         }) ;
         // Inflate the layout for this fragment
         return v;
+    }
+
+    private void bluetoothOn(){
+        if (!mBTAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            Toast.makeText(getContext(),getString(R.string.sBTturON),Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(getContext(),getString(R.string.BTisON), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void listPairedDevices(){
+        mBTArrayAdapter.clear();
+        if(mBTAdapter==null)
+        {
+            System.out.println("mBTAdapter is null");
+            return;
+        }
+        mPairedDevices = mBTAdapter.getBondedDevices();
+        if(mPairedDevices==null)
+        {
+            System.out.println("mPairedDevices is null");
+            return;
+        }
+        if(mBTAdapter.isEnabled()) {
+            // put it's one to the adapter
+            for (BluetoothDevice device : mPairedDevices)
+                mBTArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+
+            Toast.makeText(getContext(), getString(R.string.show_paired_devices), Toast.LENGTH_SHORT).show();
+        }
+        else
+            Toast.makeText(getContext(), getString(R.string.BTnotOn), Toast.LENGTH_SHORT).show();
+    }
+
+    private AdapterView.OnItemClickListener mDeviceClickListener = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            if(!mBTAdapter.isEnabled()) {
+                Toast.makeText(getContext(), getString(R.string.BTnotOn), Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            Toast.makeText(getContext(), getString(R.string.cConnet), Toast.LENGTH_LONG).show();
+            // Get the device MAC address, which is the last 17 chars in the View
+            String info = ((TextView) view).getText().toString();
+            final String address = info.substring(info.length() - 17);
+            final String name = info.substring(0,info.length() - 17);
+
+            // Spawn a new thread to avoid blocking the GUI one
+            new Thread()
+            {
+                @Override
+                public void run() {
+                    boolean fail = false;
+
+                    BluetoothDevice device = mBTAdapter.getRemoteDevice(address);
+
+                    try {
+                        mBTSocket = createBluetoothSocket(device);
+                    } catch (IOException e) {
+                        fail = true;
+                        Toast.makeText(getContext(), getString(R.string.ErrSockCrea), Toast.LENGTH_SHORT).show();
+                    }
+                    // Establish the Bluetooth socket connection.
+                    try {
+                        mBTSocket.connect();
+                    } catch (IOException e) {
+                        try {
+                            fail = true;
+                            mBTSocket.close();
+                            mHandler.obtainMessage(CONNECTING_STATUS, -1, -1)
+                                    .sendToTarget();
+                        } catch (IOException e2) {
+                            //insert code to deal with this
+                            Toast.makeText(getContext(), getString(R.string.ErrSockCrea), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    if(!fail) {
+                        mConnectedThread = new ConnectedThread(mBTSocket, mHandler);
+                        mConnectedThread.start();
+
+                        mHandler.obtainMessage(CONNECTING_STATUS, 1, -1, name)
+                                .sendToTarget();
+                    }
+                }
+            }.start();
+        }
+    };
+
+    private BluetoothSocket createBluetoothSocket(BluetoothDevice device) throws IOException {
+        try {
+            final Method m = device.getClass().getMethod("createInsecureRfcommSocketToServiceRecord", UUID.class);
+            return (BluetoothSocket) m.invoke(device, BT_MODULE_UUID);
+        } catch (Exception e) {
+            Log.e(TAG, "Could not create Insecure RFComm Connection",e);
+        }
+        return  device.createRfcommSocketToServiceRecord(BT_MODULE_UUID);
     }
 }
--- app/src/main/java/com/example/emptyapplication/MainActivity.java
+++ app/src/main/java/com/example/emptyapplication/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.emptyapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -20,6 +28,7 @@ import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.Set;
+import java.util.UUID;
 
 import com.example.emptyapplication.ui.main.SectionsPagerAdapter;
 import com.example.emptyapplication.databinding.ActivityMainBinding;
@@ -36,6 +45,11 @@ public class MainActivity extends AppCompatActivity {
         // hide appbar
         getSupportActionBar().hide();
 
+        // Ask for location permission if not already allowed
+        if(ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -45,72 +59,7 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabs = binding.tabs;
         tabs.setupWithViewPager(viewPager);
 
-        System.out.println("블루투스 client를 가져온다");
-        BluetoothSerialClient client = BluetoothSerialClient.getInstance();
-        if(client == null) {
-            // 블루투스를 사용할 수 없는 장비일 경우 null.
-            Toast.makeText(getApplicationContext(), "블루투스를 사용할 수 없는 기기입니다.", Toast.LENGTH_LONG).show();
-            System.out.println("블루투스를 사용할 수 없는 기기입니다");
-        }
-        if(!client.isEnabled()) {
-            System.out.println("블루투스 사용을 활성화 한다");
-            // 블루투스 사용을 활성화 한다.
-            // 사용자에게 블루투스 사용을 묻는 시스템 창을 출력하게 된다.
-            //블루투스가 사용가능한 상태이면 무시한다.
-            client.enableBluetooth(MainActivity.this,
-                    new BluetoothSerialClient.OnBluetoothEnabledListener() {
-                        @Override
-                        public void onBluetoothEnabled(boolean success) {
-                            System.out.println("사용자가 블루투스 사용하기를 희망하지 않음");
-                            // sucess 가 false 일 경우 사용자가 블루투스 사용하기를 희망하지 않음.
-                        }
-                    });
-        }
-
-        final ArrayList<BluetoothDevice> deviceList = new ArrayList<BluetoothDevice>();
-        // 이미 페어링된 디바이스 리스트를 가져온다.
-        Set<BluetoothDevice> pairedDevices =  client.getPairedDevices();
-        if(pairedDevices != null)
-        {
-            deviceList.addAll(pairedDevices);
-        }
-        else
-        {
-            System.out.println("페어링된 디바이스가 없다");
-        }
-        // 근처 디바이스를 스캔한다.
-        client.scanDevices(this, new BluetoothSerialClient.OnScanListener() {
-            @Override
-            public void onStart() {
-                // 스캔 시작.
-                System.out.println("스캔 시작");
-            }
-
-            @Override
-            public void onFoundDevice(BluetoothDevice bluetoothDevice) {
-                // 스캔이 완료된 디바이스를 받아온다.
-                if(deviceList.contains(bluetoothDevice)) {
-                    deviceList.remove(bluetoothDevice);
-                }
-                deviceList.add(bluetoothDevice);
-                try {
-                    // 아이템 추가.
-                    System.out.println(bluetoothDevice.getName());
-                }
-                catch(SecurityException e) {
-                    // TODO
-                }
-            }
-
-            @Override
-            public void onFinish() {
-                // 스캔 종료.
-                System.out.println("스캔 종료");
-            }
-        });
-
         // 아래와 같이 스캔 도중에 취소할 수 있다.
         //client.cancelScan(this)
-        System.out.println("onCreate 종료");
     }
 }
